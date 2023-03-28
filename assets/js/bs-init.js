178bec27fc0ca9e32903f4717f5adabb2fb603f8
riiqan <114735312+riiqan@users.noreply.github.com>
2023-03-29 07:51:12 +1200

    Add files via upload

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -24,4 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	  return new bootstrap.Tooltip(tooltipTriggerEl);
 	})
+
+	var products = document.querySelectorAll('[data-bss-dynamic-product]');
+
+	for (var product of products) {
+		var param = product.dataset.bssDynamicProductParam;
+		product.dataset.reflowProduct = new URL(location.href).searchParams.get(param)
+	}
+
 }, false);
